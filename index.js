26f75fc6848242040f611e9275a3620c47bba41e
tor <torfrancis447@gmail.com>
2022-11-05 16:43:37 -0400

    commit tree 7e01b4a51751cbb4ad3834286a5fc0a8dc741bd4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    const result =  record.find(record => record.result === "W");
+    return result ? result.year : undefined ;
+    // if(result) {
+    //     return result.year
+    // } else {
+    // return result
+    // }
+}
+
+
